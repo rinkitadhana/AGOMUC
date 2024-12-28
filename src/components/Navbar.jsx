67e531021ade6bfc126d1a0a214ae952324bc31c
Rinkit Adhana <therinkit@gmail.com>
2024-12-29 02:53:26 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,29 @@
 import { useState } from "react"
 import { IoMenu } from "react-icons/io5"
+import { navLinks } from "../Data/Data"
+import LocalTime from "../utils/LocalTime"
 
 const Navbar = () => {
   const [show, setShow] = useState(false)
   return (
-    <section className=" md:p-4 p-3 ">
-      <nav className="flex justify-between items-center border-2 border-white/30 py-3 md:px-6 px-4 rounded-full md:w-[80%] mx-auto transition-all  backdrop-blur-sm hover:backdrop-blur-xl">
-        <h1 className=" text-lg font-popins ">Agomuc</h1>
-        <div className="md:flex gap-4 hidden ">
-          <a className=" cursor-pointer font-medium hover:text-green-400 ">
-            About
+    <nav className=" font-inter select-none text-white flex justify-between items-center py-3.5 px-40 border-b border-zinc-700 bg-black/70 backdrop-blur-sm ">
+      <h1 className=" text-lg font-medium">AGOMUC</h1>
+      <div className=" flex items-center gap-8">
+        {navLinks.map((nav) => (
+          <a
+            className=" opacity-60  hover:opacity-100  "
+            key={nav.id}
+            href={nav.link}
+          >
+            {nav.name}
           </a>
-          <a className=" cursor-pointer font-medium hover:text-green-400 ">
-            Service
-          </a>
-          <a className=" cursor-pointer font-medium hover:text-green-400 ">
-            Contact
-          </a>
-        </div>
-        <div
-          onClick={() => setShow((prev) => !prev)}
-          className=" md:hidden text-3xl "
-        >
-          <IoMenu />
-        </div>
-      </nav>
-      {show && (
-        <div className="flex flex-col gap-4 border-2 rounded-xl my-2 p-3 md:hidden backdrop-blur-sm border-white/30">
-          <a className=" cursor-pointer font-medium ">About</a>
-          <a className=" cursor-pointer font-medium ">Service</a>
-          <a className=" cursor-pointer font-medium ">Contact</a>
-        </div>
-      )}
-    </section>
+        ))}
+      </div>
+      <div className=" flex gap-1 items-center text-sm  font-bold">
+        <span>Local Time</span>
+        <LocalTime />
+      </div>
+    </nav>
   )
 }
 
