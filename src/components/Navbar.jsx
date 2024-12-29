32184e6ccc73f8b3a67d458c5794053e650e9c9e
Rinkit Adhana <therinkit@gmail.com>
2024-12-29 20:42:00 +0530

    Refactor Navbar component: update layout to fixed position, enhance styling with new font and background, and add 'Get Started' button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,27 @@ import LocalTime from "../utils/LocalTime"
 const Navbar = () => {
   const [show, setShow] = useState(false)
   return (
-    <nav className=" font-inter select-none text-white flex justify-between items-center py-3.5 px-40 border-b border-zinc-700 bg-black/70 backdrop-blur-sm ">
-      <h1 className=" text-lg font-medium">AGOMUC</h1>
-      <div className=" flex items-center gap-8">
-        {navLinks.map((nav) => (
-          <a
-            className=" transition duration-300 opacity-60  hover:opacity-100  "
-            key={nav.id}
-            href={nav.link}
-          >
-            {nav.name}
-          </a>
-        ))}
-      </div>
-      <div className="  flex gap-1 items-center text-sm  font-medium opacity-90">
+    <nav className=" fixed top-0 w-full z-10">
+      <div className=" font-sans  select-none text-white flex justify-between items-center py-2.5 px-40 border-b border-zinc-800 bg-black/50 backdrop-blur-md ">
+        <h1 className=" text-lg font-popins font-medium">Agomuc.</h1>
+        <div className=" flex items-center gap-8">
+          {navLinks.map((nav) => (
+            <a
+              className=" font-inter text-sm transition duration-300 opacity-60  hover:opacity-100  "
+              key={nav.id}
+              href={nav.link}
+            >
+              {nav.name}
+            </a>
+          ))}
+        </div>
+        {/* <div className="  flex gap-1 items-center text-sm  font-medium opacity-60">
         <span>Local Time</span>
         <LocalTime />
+      </div> */}
+        <a className="  transition duration-300 cursor-pointer bg-blue-700 rounded-full px-2.5  py-1 border border-blue-600 hvr-sdw">
+          Get Started
+        </a>
       </div>
     </nav>
   )
