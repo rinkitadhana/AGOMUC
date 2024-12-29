fed1289fe7cc85d7cf3696014dae11fdc42d01c1
Rinkit Adhana <therinkit@gmail.com>
2024-12-29 20:41:54 +0530

    Refactor Hero component: integrate RotatingCube, adjust layout proportions, and enhance text styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
 import { IoArrowForward } from "react-icons/io5"
+import RotatingCube from "./RotatingCube"
 
 const Hero = () => {
   return (
-    <section className="  flex gap-6 my-28 items-center">
-      <div className=" basis-[55%] flex flex-col gap-6">
+    <section className="flex gap-6 my-28 items-center">
+      <div className=" basis-[60%] flex flex-col gap-6">
         <h1 className="text-6xl font-semibold text-white/80">AGOMUC PROGRAM</h1>
-        <p className=" text-lg text-zinc-200">
+        <p className="text-lg text-zinc-200 font-medium opacity-75">
           At AGOMUC, we leverage advanced AI and ML to transform crime
           prevention, providing real-time analytics and safety features to
           enhance community security.
         </p>
         <div className=" select-none flex gap-8 items-center">
-          <a className="transition duration-300 cursor-pointer bg-blue-600 rounded-full px-3.5  py-2 border border-blue-600 hover:border-blue-300">
+          <a className="transition duration-300 cursor-pointer bg-blue-700 rounded-full px-3.5  py-2 border border-blue-600 hvr-sdw">
             About us
           </a>
           <a className=" transition duration-300 group/arr flex gap-2 items-center cursor-pointer bg-black hover:bg-zinc-800 border border-black hover:border-zinc-800 px-3.5  py-2  rounded-full">
@@ -22,11 +23,8 @@ const Hero = () => {
           </a>
         </div>
       </div>
-      <div className=" select-none  basis-[45%]">
-        <img
-          className="  rounded-3xl"
-          src="https://img.freepik.com/premium-photo/face-young-woman-face-white-background-generative-ai_849906-20048.jpg"
-        />
+      <div className=" select-none  basis-[40%]">
+        <RotatingCube />
       </div>
     </section>
   )
