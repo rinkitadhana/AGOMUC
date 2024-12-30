309a2a8a88c343ef44f39f0bb0e73c3eb437283d
Rinkit Adhana <therinkit@gmail.com>
2024-12-30 14:55:42 +0530

    head title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 const Title = ({ title, des }) => {
   return (
     <div className=" flex flex-col gap-1 items-center">
-      <h1 className=" text-4xl font-medium">{title}</h1>
+      <h1 className=" sec-head ">{title}</h1>
       <p className=" text-center text-sm font-medium opacity-70">{des}</p>
     </div>
   )
