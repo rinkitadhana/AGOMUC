684212e627bc03372457337b4713d861ab0c01ae
Rinkit Adhana <therinkit@gmail.com>
2024-12-31 16:09:42 +0530

    Update Home component: integrate Footer, Solutions, Sample, and Sample2 components; adjust layout spacing for improved design and responsiveness.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,26 @@
 import About from "../components/About"
+import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
+import Sample from "../components/Sample"
+import Sample2 from "../components/Sample2"
+import Solutions from "../components/Solutions"
 import Screen from "./../Layout/Screen"
 const Home = () => {
   return (
-    <>
+    <div className=" select-none">
       <Navbar />
       <Screen>
-        <div className=" flex flex-col gap-8">
+        <div className=" flex flex-col gap-10">
           <Hero />
           <About />
-          <div className=" h-screen"></div>
+          <Solutions />
+          <Sample />
+          <Sample2 />
         </div>
       </Screen>
-    </>
+      <Footer />
+    </div>
   )
 }
 
