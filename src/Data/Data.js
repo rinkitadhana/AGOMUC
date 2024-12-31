9fede66ca99be94308bcf0ce96ef192922f950d8
Rinkit Adhana <therinkit@gmail.com>
2024-12-31 16:08:50 +0530

    Add sampleData array to Data.js: include multiple entries with icons and descriptions for enhanced data representation

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,3 +1,5 @@
+import { GiSelfLove } from "react-icons/gi"
+
 const navLinks = [
   {
     id: 1,
@@ -20,5 +22,43 @@ const navLinks = [
     link: "/",
   },
 ]
+const sampleData = [
+  {
+    id: 1,
+    icon: GiSelfLove,
+    title: "Dashboards for all businesses",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam delectus rerum, reiciendis quisquam cum sunt.",
+  },
+  {
+    id: 2,
+    icon: GiSelfLove,
+    title: "Dashboards for all businesses",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam delectus rerum, reiciendis quisquam cum sunt.",
+  },
+  {
+    id: 3,
+    icon: GiSelfLove,
+    title: "Dashboards for all businesses",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam delectus rerum, reiciendis quisquam cum sunt.",
+  },
+  {
+    id: 4,
+    icon: GiSelfLove,
+    title: "Dashboards for all businesses",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam delectus rerum, reiciendis quisquam cum sunt.",
+  },
+  {
+    id: 5,
+    icon: GiSelfLove,
+    title: "Dashboards for all businesses",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam delectus rerum, reiciendis quisquam cum sunt.",
+  },
+  {
+    id: 6,
+    icon: GiSelfLove,
+    title: "Dashboards for all businesses",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit totam delectus rerum, reiciendis quisquam cum sunt.",
+  },
+]
 
-export { navLinks }
+export { navLinks, sampleData }
